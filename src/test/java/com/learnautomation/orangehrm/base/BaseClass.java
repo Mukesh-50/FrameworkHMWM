9cf1985c33bf982743f3c1eb634f7e7b31ef8dd9
Mukesh Otwani <mukeshotwani.50@gmail.com>
2021-04-16 10:34:07 +0530

    framework changes with rest assured

--- src/test/java/com/learnautomation/orangehrm/base/BaseClass.java
+++ src/test/java/com/learnautomation/orangehrm/base/BaseClass.java
@@ -3,6 +3,7 @@ package com.learnautomation.orangehrm.base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import com.learnautomation.orangehrm.utility.BrowserFactory;
@@ -17,7 +18,7 @@ public class BaseClass
 		return driver;
 	}
 	
-	@Parameters({"Browser","stagingURL"})
+	/*@Parameters({"Browser","stagingURL"})
 	@BeforeClass
 	public void startBrowser(String Browser,String url)
 	{
@@ -26,6 +27,24 @@ public class BaseClass
 		BrowserFactory browser=new BrowserFactory();
 		
 		driver=browser.openBrowser(Browser,url);
+		
+		System.out.println("**** Browser is up and running ****");
+	}*/
+	
+	@Parameters({"local","BrowserName","BrowserVersion","OsName","OsVersion","stagingURL"})
+	@BeforeClass
+	public void startBrowser(@Optional("true")boolean local,@Optional("Chrome")String BrowserName,@Optional("latest")String BrowserVersion,@Optional("Windows")String OsName,@Optional("8.1")String OsVersion,String url)
+	{
+		System.out.println("**** Starting Browser On BrowserStack  with below details****");
+		
+		System.out.println("Browser Name "+BrowserName);
+		System.out.println("Browser Version "+BrowserVersion);
+		System.out.println("OS Name "+OsName);
+		System.out.println("OS Version "+OsVersion);
+		
+		BrowserFactory browser=new BrowserFactory();
+		
+		driver=browser.openBrowser(local,BrowserName, BrowserVersion, OsName, OsVersion, url);
 		
 		System.out.println("**** Browser is up and running ****");
 	}
